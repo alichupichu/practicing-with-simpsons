5d7a46db16f1d05644815f4b5abe88732bbf5569
Alicia Mendez Cruz <aliciamendezcruz@Alichus-MacBook-Pro-16.local>
2024-02-22 18:58:02 +0000

    routes added

---
+++ src/components/Error.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Error = () => {
+  return <p>Page not found</p>;
+};
+
+export default Error;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>Copyright 2024 AMC</small>
+    </footer>
+  );
+};
+
+export default Footer;
